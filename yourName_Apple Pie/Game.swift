6176ce418e130bf18079a9e5c15f31b09bc5be5b
Mason Guaman <masonstalin@gmail.com>
2022-10-18 14:17:46 -0400

    fd

---
+++ yourName_Apple Pie/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  yourName_Apple Pie
+//
+//  Created by 5e on 10/18/22.
+//
+
+import Foundation
+struct Game {
+var word: String
+var incorrectMovesRemaining: Int
+}
--- yourName_Apple Pie/ViewController.swift
+++ yourName_Apple Pie/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalLosses = 0
+    
+    var totalWins = 0
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    var currentGame: Game!
+    
+    func updateUI() {
+    scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+    treeImageView.image = UIImage(named: "Tree  \(currentGame.incorrectMovesRemaining)")
 
+    }
+
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
